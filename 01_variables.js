e3f8f36500cf24ed2a6c35d1f390b71c1f62374a
Eldohub Academy <academy@eldohub.co.ke>
2024-11-11 11:33:11 +0300

    first class: variables and datatypes

---
+++ 01_variables.js
@@ -0,0 +1,15 @@
+// variables are used to store data in memory when your application/software is running
+// in we can use var, let or const to create variables
+
+var user = "albert"
+let score = 90
+
+const age = 21
+
+// var was the only way used to decalre variables in js before 2015 (es6) and it had some flaws
+//es6 introduced let and const 
+// let is used for variables that can change as the program is running e.g. game scores
+// const is used to declare/create non-changing/constant variables i.e. do no change as the program is running e.g. PI, user_role, 
+age = 80
+
+score = 100
---
+++ 02_datatypes.js
@@ -0,0 +1,30 @@
+// datatypes determine what kind of values to stored in memory.
+
+// in js we can split data types in two categories
+// 1. Primitives/simple :- number, string, boolean, undefined , null
+// 2. Non-primitives/complex/composite:- arrays, objects, functions
+
+let rate = 17.8; // number
+let password = "jhemkfs36742#$3!"; // string
+const isAdmin = true; //boolean
+let isLoggedIn = false; //boolean
+
+// when you create a variable and not assign it a value, by default it is undefined
+let username;
+
+console.log(username); //undefined
+
+// when you want to intentionally set a space to be empty, you  use null e.g. totalScore :- if the game has not started then we cant have a totalscore
+let totalScore = null;
+
+// non primitves
+// array -- ordered list of values (same or different data types)
+const colors = ["red", "green", "blue"];
+const someVals = [true, 563.5, "okay", null, [254, 256]];
+
+// ASSIGNMENT:- Why are arrays indexed and how do we access values in an array?
+
+// objects
+// functions
+// input/output
+//operators
